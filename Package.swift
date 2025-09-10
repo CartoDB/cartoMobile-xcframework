e04a07d4be47435348f05e4e300287def5eb52a7
Joaquin Perez Barroso <jperbar@me.com>
2025-09-10 12:28:45 +0200

    Version 4.4.8

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "CartoMobileSDK", url: "https://storage.googleapis.com/aena-xcframework/ios-mobile-carto-4.6.zip", checksum: "b5506ebc8792b99a5182a65b31bf7eb1edfac4d0aa0ab536625f2d487e04b759")
+        .binaryTarget(name: "CartoMobileSDK", url: "https://storage.googleapis.com/aena-xcframework/ios-mobile-carto-4.8.zip", checksum: "4da0c6b3fa0232de0bb0d949f9290b3951c70cecf28cd09abaca6b9c986509c6")
 
     ]
 )
