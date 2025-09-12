330a66a416210f066b58019c21925f0cf6866945
Joaquin Perez Barroso <jperbar@me.com>
2025-09-12 10:55:00 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CartoMobileSDK",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v9)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
