aaf096184923e789996bdfbebbb85f8ca3ad6afb
Joaquín Pérez Barroso <joaquinpbarroso@CartoMac-de-Joaquin.local>
2025-09-12 12:25:46 +0200

    Version 4.5.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "CartoMobileSDK", url: "https://storage.googleapis.com/aena-xcframework/ios-mobile-carto-4.8.zip", checksum: "4da0c6b3fa0232de0bb0d949f9290b3951c70cecf28cd09abaca6b9c986509c6")
+        .binaryTarget(name: "CartoMobileSDK", url: "https://storage.googleapis.com/aena-xcframework/ios-mobile-carto-5.0.zip", checksum: "7a07454b710abe539b62141ce0722588915c1feb701cf8826c03ae9a4aa4e4ff")
 
     ]
 )
